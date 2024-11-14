96ef72c0eba2a5eb941bdeedb28ef15ee40582cf
Cong Binh <truymenhcm@gmail.com>
2024-11-14 23:53:45 +0700

    update code project

---
+++ ShopProducts/ShopPhone/Library/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopPhone.Library
+{
+    public class Util
+    {
+        // email dung de gui di
+        public static string email ="NgoCongBinh@gmail.com";
+        //pass email
+        public static string password = "NgoCongBinh";
+    }
+}
